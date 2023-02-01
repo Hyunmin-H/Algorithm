39e21863f1088f35fb56b0e6d79a66dc4bc720ad
한현민 <33598545+Hyunmin-H@users.noreply.github.com>
2023-02-01 13:10:26 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 124.61 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/README.md
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/README.md
@@ -1,10 +1,10 @@
 # [unrated] 표현 가능한 이진트리 - 150367 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367#) 
 
 ### 성능 요약
 
-메모리: 10.8 MB, 시간: 97.73 ms
+메모리: 10.7 MB, 시간: 124.61 ms
 
 ### 구분
 
--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,10 +1,17 @@
-import sys
+
 import math
+import sys
 sys.setrecursionlimit(10**6)
 def solution(numbers):
     answer = []
     
-    def makeBinaryNumber(n):
+    # def digitTo2(n):
+    #     output = ''
+    #     while n>0:
+    #         output = str(n%2) + output
+    #         n = n //2
+    #     return output
+    def digitTo2(n):
         bin_num = ''
         while n >=2 :
             bin_num = str(n%2) + bin_num
@@ -12,14 +19,31 @@ def solution(numbers):
         bin_num = str(n) + bin_num
         return bin_num
     
-    def calcLengthOfPohwaTree(length_bin_num):
+    def BinToPohwabin(bin_num):
+        pohwa_length = 2 ** (math.ceil(math.log(len(bin_num)) / math.log(2) )) - 1
+        return '0' * (pohwa_length - len(bin_num)) + bin_num
+    
+    def calcLengthOfPohwaTree(bin_num):
+        len_pohwa = 2 ** (math.floor(math.log(len(bin_num)) / math.log(2) + 1 ))-1
+        bin_num  = '0' * (len_pohwa - len(bin_num)) + bin_num
+        return bin_num
+    def binarySearch(start, end, pohwa_num):
+        if start >= end : 
+            return pohwa_num[start]
         
-        return 2 ** (math.floor(math.log(length_bin_num) / math.log(2) + 1 ))-1
+        mid = (start+end) // 2
+        left = binarySearch(start, mid-1, pohwa_num)
+        right = binarySearch(mid+1, end, pohwa_num)
         
-        
-        
+        if left is False or right is False :
+            return False
+        if pohwa_num[mid] == '0' and (left == '1' or right == '1'): 
+            return False
+        if left =='0' and right =='0' and pohwa_num[mid] == '0' :
+            return '0'
+        return '1'
+    
     def checkBinaryTree(start, end, bin_num): 
-        # print('start', start, 'end', end)
         if start == end  : 
             return bin_num[start] 
         
@@ -32,19 +56,14 @@ def solution(numbers):
             return False
         if left == '0' and right == '0' and bin_num[mid] == '0': return '0'
         return '1'
-            
-    for n in numbers :
-        bin_num = makeBinaryNumber(n)
-        len_pohwa = calcLengthOfPohwaTree(len(bin_num))
         
-        bin_num  = '0' * (len_pohwa - len(bin_num)) + bin_num
         
-        # print('n', n, 'bin_num', bin_num)
-        # print('len_pohwa', len_pohwa)
-        if checkBinaryTree(0, len_pohwa-1, bin_num) : 
+    
+    for num in numbers:
+        bin_num = digitTo2(num)
+        pohwa_num = calcLengthOfPohwaTree(bin_num)
+        if binarySearch(0, len(pohwa_num)-1, pohwa_num) : 
             answer.append(1)
         else :
             answer.append(0)
-    
-    print('answer', answer)
     return answer
