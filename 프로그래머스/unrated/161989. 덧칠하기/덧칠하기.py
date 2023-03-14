57b3cdc706e1a439eac199d4d356379eee2956df
한현민 <33598545+Hyunmin-H@users.noreply.github.com>
2023-03-14 15:45:48 +0900

    [unrated] Title: 덧칠하기, Time: 29.25 ms, Memory: 12.8 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -1,17 +1,18 @@
 def solution(n, m, section):
-    answer = 1
+    answer = 0
     
     wall = [0 for _ in range(n+1)]
-    remain = m
     for n in section : 
         wall[n] = 1
     
+    ans1 = 1
+    remain = m
     for i in range(1, n+1):
         if wall[i] : 
             if remain > 0 : 
                 remain -= 1
             else : 
-                answer += 1
+                ans1 += 1
                 remain = m-1
         else : 
             remain = max(0, remain-1)
@@ -28,4 +29,4 @@ def solution(n, m, section):
         else : 
             remain = max(0, remain-1)
             
-    return min(answer, ans2)
+    return min(ans1, ans2)
