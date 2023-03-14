63e1ea6425cb366334d2c1a42a4d5f3d25119183
한현민 <33598545+Hyunmin-H@users.noreply.github.com>
2023-03-14 15:47:29 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.88 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -9,27 +9,27 @@ def solution(maps):
     dy = [-1, 1, 0, 0]
     dx = [0, 0, -1, 1]
     
-    def bfs(i, j): 
+    def bfs(i_, j_): 
         
         v = [[False for _ in range(W)] for _ in range(H)] 
         dist = [[1 for _ in range(W)] for _ in range(H)]
         
-        q = deque([[i, j]])
+        q = deque([[i_, j_]])
         
         while q : 
-            ii, jj = q.popleft()
+            i, j = q.popleft()
             
             for n in range(4):
-                iii = ii + dy[n]
-                jjj = jj + dx[n]
+                ii = i + dy[n]
+                jj = j + dx[n]
                 
-                if iii < 0 or jjj < 0 or iii >= H or jjj >= W or maps[iii][jjj] == 0:
+                if ii < 0 or jj < 0 or ii >= H or jj >= W or maps[ii][jj] == 0:
                     continue
                     
-                if not v[iii][jjj] : 
-                    dist[iii][jjj] = dist[ii][jj] + 1
-                    v[iii][jjj] = True
-                    q.append([iii, jjj])
+                if not v[ii][jj] : 
+                    dist[ii][jj] = dist[i][j] + 1
+                    v[ii][jj] = True
+                    q.append([ii, jj])
         
         if dist[H-1][W-1] == 1 : 
             return -1
