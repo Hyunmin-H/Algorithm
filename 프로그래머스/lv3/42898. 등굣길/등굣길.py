b36d75858e55e131f3a836cbc1464d33252f295e
한현민 <33598545+Hyunmin-H@users.noreply.github.com>
2023-08-06 19:03:58 +0900

    [level 3] Title: 등굣길, Time: 1.75 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -0,0 +1,41 @@
+def print_2d(a):
+    for i in a:
+        for j in i : 
+            print(j, end=' ')
+        print()
+
+def solution(m, n, puddles):
+    answer = 0
+    
+    ans_2d = [[0 for _ in range(m)] for _ in range(n)]
+    
+    for j, i in puddles:
+        ans_2d[i-1][j-1] = -1
+    
+    is_puddle = False
+    for i in range(n):
+        if ans_2d[i][0] == -1 : 
+            is_puddle = True
+        if is_puddle : 
+            ans_2d[i][0] = 0
+        else : 
+            ans_2d[i][0] = 1
+    
+    is_puddle = False 
+    for j in range(m):
+        if ans_2d[0][j] == -1 :
+            is_puddle = True
+        if is_puddle :
+            ans_2d[0][j] = 0
+        else : 
+            ans_2d[0][j] = 1
+    
+    for i in range(1, n):
+        for j in range(1, m):
+            if ans_2d[i][j] == -1 :
+                ans_2d[i][j] = 0
+                continue
+            ans_2d[i][j] = (ans_2d[i-1][j] + ans_2d[i][j-1]) % 1000000007
+                
+    
+    return ans_2d[n-1][m-1]
